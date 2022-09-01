367a3ac2843be61cf99a2c2734ef329815d32a42
AnselYe <102567414+AnselYee@users.noreply.github.com>
2022-09-01 18:29:26 -0500

    ch2inclass1
    height calculate

---
+++ .github/src/inclasschapter2/ch2inclass1.java
@@ -0,0 +1,17 @@
+package inclasschapter2;
+
+public class ch2inclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int height1=65;
+		int height2=61;
+		int height3=67;
+		int height4=74;
+		int height5=70;
+		double Sumheight=height1+height2+height3+height4+height5;
+		double aveheight=Sumheight/5;
+		System.out.println("The average height is "+ aveheight+" inches.");
+	}
+
+}
