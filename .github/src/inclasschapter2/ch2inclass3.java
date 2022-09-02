3c5c62e1e9d569b5e2331b07f5dc308b1b3fa5a2
AnselYe <102567414+AnselYee@users.noreply.github.com>
2022-09-01 20:38:24 -0500

    Create ch2inclass3.java

---
+++ .github/src/inclasschapter2/ch2inclass3.java
@@ -0,0 +1,12 @@
+package inclasschapter2;
+
+public class ch2inclass3 {
+final int EXAMPLE=4;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int EXAMPLE=5;
+		System.out.println(EXAMPLE);
+	}
+
+}
